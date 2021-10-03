f4f2a09f801d511e06f6442ea44ed89b5f472971
TimBaechle <st167752@stud.uni-stuttgart.de>
2021-10-03 15:03:22 +0200

    some more stuff

--- lib/widgets/workoutTile.dart
+++ lib/widgets/workoutTile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:workout/dBService.dart';
 import 'package:workout/model/exercise.dart';
 import 'package:workout/model/workout.dart';
+import 'package:workout/pages/exerciseView.dart';
 
 class WorkoutTile extends StatefulWidget {
   const WorkoutTile({Key? key, required this.workout}) : super(key: key);
@@ -19,34 +20,40 @@ class _WorkoutTileState extends State<WorkoutTile> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            GestureDetector(
-              onTap: onWorkoutTap,
-              child: Row(
-                children: [
-                  isExtended
-                      ? Icon(Icons.keyboard_arrow_up)
-                      : Icon(Icons.keyboard_arrow_down),
-                  Expanded(
-                      child: Text(
-                    widget.workout.name,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 25),
-                  )),
-                  isExtended
-                      ? Icon(Icons.keyboard_arrow_up)
-                      : Icon(Icons.keyboard_arrow_down)
-                ],
+    return Container(
+      margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: onWorkoutTap,
+                child: Row(
+                  children: [
+                    isExtended
+                        ? Icon(Icons.keyboard_arrow_up)
+                        : Icon(Icons.keyboard_arrow_down),
+                    Expanded(
+                        child: Text(
+                      widget.workout.name,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 25),
+                    )),
+                    isExtended
+                        ? Icon(Icons.keyboard_arrow_up)
+                        : Icon(Icons.keyboard_arrow_down)
+                  ],
+                ),
               ),
-            ),
-            isExtended
-                ? Column(children: [Text('Extended')])
-                : Container(height: 0.0)
-          ],
+              isExtended
+                  ? isLoadingExercises
+                      ? CircularProgressIndicator()
+                      : Column(children: _getExtendedPart())
+                  : Container(height: 0.0)
+            ],
+          ),
         ),
       ),
     );
@@ -70,5 +77,32 @@ class _WorkoutTileState extends State<WorkoutTile> {
         isLoadingExercises = false;
       });
     }
+  }
+
+  /// Returns the bottom part of a workout Tile (The part, that is only visible when extended)
+  List<Widget> _getExtendedPart() {
+    List<Widget> extensionBody = [
+      Divider(indent: 75, endIndent: 75, thickness: 2, color: Colors.white)
+    ];
+
+    exercises!.forEach((exercise) {
+      extensionBody.add(GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => ExerciseView()));
+        },
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Expanded(
+                  child: Text(exercise.name, style: TextStyle(fontSize: 20))),
+              Icon(Icons.arrow_forward_rounded)
+            ],
+          ),
+        ),
+      ));
+    });
+    return extensionBody;
   }
 }
